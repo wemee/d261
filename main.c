492a54420545f134e302e0e84db1f94ccb1037d3
Jr-Yung Tsai <wemee7012@gmail.com>
2015-03-06 11:17:50 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include "stdio.h"
 
+#define _Female_ 0
+#define _Male_ 1
+
 int main(){
     int n;
     unsigned int ary[2][2], i, previous, current;
@@ -8,16 +11,21 @@ int main(){
         n = (unsigned int)n;
         
         current=0;
-        ary[current][0] = 1; /* 初始母峰 1 */
-        ary[current][1] = 0; /* 初始公峰 0 */
+        ary[current][_Female_] = 1;/* 初始母蜂 1 */
+        ary[current][_Male_] = 0;/* 初始公蜂 0 */
+        
         for (i=0; i<n; i++) {
             previous=current;
             ++current, current%=2;
             
-            ary[current][0] = 1/* 初始母峰不死 */ + ary[previous][1]/* 所有公峰生1隻母蜂 */;
-            ary[current][1] = ary[previous][0] + ary[previous][1];/* 所有蜜峰都生1隻公蜂 */
+            /* 初始母蜂不死，並且所有公蜂都生1隻母蜂 */
+            ary[current][_Female_] = 1 + ary[previous][_Male_];
+            
+            /* 所有蜜蜂都生1隻公蜂 */
+            ary[current][_Male_] = ary[previous][_Female_] + ary[previous][_Male_];
         }
-        printf("%u %u\n", ary[current][1], ary[current][0]+ary[current][1]);
+        
+        printf("%u %u\n", ary[current][_Male_], ary[current][_Female_]+ary[current][_Male_]);
     }
     return 0;
 }
