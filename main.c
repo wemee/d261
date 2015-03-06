d63612f8b10b47540041c64a3af2726201e18b4c
Jr-Yung Tsai <wemee7012@gmail.com>
2015-03-06 10:58:58 +0800

    Update main.c

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ int main(){
         ary[current][1] = 0; /* 初始公峰 0 */
         for (i=0; i<n; i++) {
             previous=current;
-            current++, current%=2;
+            ++current, current%=2;
             
             ary[current][0] = 1/* 初始母峰不死 */ + ary[previous][1]/* 所有公峰生1隻母蜂 */;
             ary[current][1] = ary[previous][0] + ary[previous][1];/* 所有蜜峰都生1隻公蜂 */
